9846d8d3a4a211d90022ef86e89f9abc69638e30
Daniel Villalba <1821@holbertonschool.com>
2020-06-10 20:02:03 +0000

    Fized

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -6,15 +6,16 @@
  */
 int main(void)
 {
-  long long type_llong;
-  char type_char;
-  long int type_lint;
-  float type_fl;
-  int type_int;
-  printf("Size of a char: %lu byte(s)\n", sizeof(type_char));
-  printf("Size of an int: %lu byte(s)\n", sizeof(type_int));
-  printf("Size of a long int: %lu byte(s)\n", sizeof(type_lint));
-  printf("Size of a long long int: %lu byte(s)\n", sizeof(type_llong));
-  printf("Size of a float: %lu byte(s)\n", sizeof(type_fl));
-  return (0);
+long long type_llong;
+char type_char;
+long int type_lint;
+float type_fl;
+int type_int;
+
+printf("Size of a char: %lu byte(s)\n", sizeof(type_char));
+printf("Size of an int: %lu byte(s)\n", sizeof(type_int));
+printf("Size of a long int: %lu byte(s)\n", sizeof(type_lint));
+printf("Size of a long long int: %lu byte(s)\n", sizeof(type_llong));
+printf("Size of a float: %lu byte(s)\n", sizeof(type_fl));
+return (0);
 }
