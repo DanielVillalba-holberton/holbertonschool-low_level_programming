e26cafa83aa133d15d44d8b1a9aa5c3b8c1ac3ea
Daniel Villalba <1821@holbertonschool.com>
2020-06-11 22:13:47 +0000

    Fixed2

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -18,7 +18,7 @@ int main(void)
 		printf("Last digit of %i is %i and is greater than 5\n", n, a);
 	else if (a == 0)
 		printf("Last digit of %i is %i and is 0\n", n, a);
-	else if (a < 6 && a != 0)
+	else if (a < 6)
 		printf("Last digit of %i is %i and is less than 6 and not 0\n", n, a);
 	return (0);
 }
