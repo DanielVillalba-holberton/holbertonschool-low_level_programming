94993a799c84cad3d6d106f7161f19cc7012f8b0
Daniel Villalba <1821@holbertonschool.com>
2020-06-16 20:34:16 +0000

    Advanced task 4

--- 0x02-functions_nested_loops/103-fibonacci.c
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -7,16 +7,15 @@
 int main(void)
 {
 	int n = 0;
-	unsigned long a = 1, b = 2, c, sum;
-	
-	while (n < 50)
+	unsigned long a = 1, b = 2, c, sum = 0;
+
+	while (a <= 4000000)
 	{
-		if ((a < 4000000) && (a % 2 == 0))
+		if (a % 2 == 0)
 		{
 			sum = sum + a;
 		}
 		c = b, b = a + b, a = c;
-		n++;
 	}
 	printf("%lu\n", sum);
 	return (0);
