cc4cc733fb4e05f693a14070414addb56deb5e19
Daniel Villalba <1821@holbertonschool.com>
2020-06-16 14:37:52 +0000

    Advanced task 1

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,50 @@
+#include "holberton.h"
+/**
+ * print_times_table - prints table n
+ * @n: value of n tables
+ *
+ * Return: void
+ */
+void print_times_table(int n)
+{
+	int row, col, sum, t, n1, n2, n3;
+
+	if (n >= 0 && n < 16)
+	{
+		col = 0;
+		while (col <= n)
+		{
+			_putchar('0');
+			row = 1;
+			while (row <= n)
+			{
+				_putchar(',');
+				_putchar(' ');
+				sum = col * row;
+				t = sum / 10;
+				if (sum < 10)
+				{
+					_putchar(' ');
+					_putchar(sum + '0');
+				}
+				else if (sum >= 10 && sum < 100)
+				{
+					_putchar(t + '0');
+					_putchar(sum % 10 + '0');
+				}
+				else
+				{
+					n1 = (sum / 10) / 10;
+					_putchar(n1 + '0');
+					n2 = (sum / 10) % 10;
+					_putchar(n2 + '0');
+					n3 = sum % 10;
+					_putchar(n3 + '0');
+				}
+				row++;
+			}
+			_putchar('\n');
+			col++;
+		}
+	}
+}
