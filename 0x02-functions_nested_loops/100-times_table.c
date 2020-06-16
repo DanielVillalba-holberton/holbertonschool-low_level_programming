c49fd680ccd972ca57ddac3508d3cb200efb5e15
Daniel Villalba <1821@holbertonschool.com>
2020-06-16 15:34:54 +0000

    Add fixed task

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -20,19 +20,16 @@ void print_times_table(int n)
 			{
 				_putchar(',');
 				_putchar(' ');
-				sum = col * row;
-				n1 = sum / 100;
-				n2 = (sum / 10) % 10;
-				n3 = sum % 10;
+				sum = col * row, n1 = sum / 100, n2 = (sum / 10) % 10, n3 = sum % 10;
 				if (sum < 10)
 				{
-					_putchar(' ');					
+					_putchar(' ');
 					_putchar(' ');
 					_putchar(sum + '0');
 				}
 				else if (sum >= 10 && sum < 100)
 				{
-					_putchar(' ');					
+					_putchar(' ');
 					_putchar(n2 + '0');
 					_putchar(n3 + '0');
 				}
