a1237d5e2afd4b18ffc792f41cba2875e4754751
Daniel Villalba <1821@holbertonschool.com>
2020-06-18 22:24:39 +0000

    Angry fixxxxxx

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,28 +1,27 @@
-include <stdio.h>
+#include <stdio.h>
 /**
- * main - Entry point
+ * main - FizzBuzz.
  *
- * Return: Always 0 (Success)
+ * Return: Always 0.
  */
+
 int main(void)
 {
-	int i;
+	int i = 1;
 
-	for (i = 1; i <= 100; ++i)
+	for (i; i <= 100; ++i)
 	{
-		if (i % 3 == 0 && i % 5 == 0)
-			printf("FizzBuzz");
-		else if (i % 3 == 0)
+		if (i % 3 == 0)
 			printf("Fizz");
 		else if (i % 5 == 0)
 			printf("Buzz");
+		else if (i % 5 == 0 && i % 3 == 0)
+			printf("FizzBuzz");
 		else
 			printf("%d", i);
-
 		if (i != 100)
 			putchar(' ');
 	}
-
 	putchar('\n');
 	return (0);
 }
