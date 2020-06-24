498ec4276de31071a52ecf61abb0184e15c706d2
Daniel Villalba <1821@holbertonschool.com>
2020-06-24 01:02:31 +0000

    fixed

--- 0x05-pointers_arrays_strings/1-swap.c
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -8,10 +8,7 @@
 void swap_int(int *a, int *b)
 {
 	int x = *a; /* look at the value save in the pointer of *a*/
-	int y = *b; /* look at the value in the ointer *b */
 
-	x = 42;
-	*a = x; /* assign the value of x in the memory position of *a*/
-	y = 98;
-	*b = y; /* asaign the value of z to the memory position of *b */
+	*a = *b; /* assign the value of x in the memory position of *a*/
+	*b = x; /* asaign the value of z to the memory position of *b */
 }
