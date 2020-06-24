4b97ada6ea7ed88b7eede41d4483a624f0b95db6
Daniel Villalba <1821@holbertonschool.com>
2020-06-24 01:18:24 +0000

    fixed

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,10 +11,13 @@ void puts_half(char *str)
 
 	while (str[i] != 0)
 		i++;
-	if (i % 2 == 0)
+	if (i > 1)
+	{
+		if (i % 2 == 0)
 		for (j = i / 2; j < i; j++)
 			_putchar(str[j]);
 	else
 		for (k = i / 2 + 1; k < i; k++)
 			_putchar(str[k]);
+	}
 }
