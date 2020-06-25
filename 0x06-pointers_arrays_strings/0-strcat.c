7b3b398174a51d1fddad179390a289bb17053dd3
Daniel Villalba <1821@holbertonschool.com>
2020-06-25 16:26:49 +0000

    fixed

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -5,18 +5,18 @@
  * @src: string 2
  * Return:dest .
  */
- char *_strcat(char *dest, char *src)
- {
-    int c = 0, j = 0;
-    
-    while (dest[c] != '\0')
-        c++;
-    while (src[j] != '\0')
-    {
-        dest[c] = src[j];
-        j++;
-        c++;
-    }
-    dest[c] = '\0';
-    return(dest);
- }
+char *_strcat(char *dest, char *src)
+{
+	int c = 0, j = 0;
+
+	while (dest[c] != '\0')
+		c++;
+	while (src[j] != '\0')
+	{
+		dest[c] = src[j];
+		j++;
+		c++;
+	}
+	dest[c] = '\0';
+	return (dest);
+}
