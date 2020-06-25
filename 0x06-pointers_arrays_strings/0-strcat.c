970e3352cad2169a445ccb2607800f17aa2bdf18
Daniel Villalba <1821@holbertonschool.com>
2020-06-25 16:23:59 +0000

    add task

---
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,22 @@
+#include "holberton.h"
+/**
+ * _strcat - append to strings
+ * @dest: string 1
+ * @src: string 2
+ * Return:dest .
+ */
+ char *_strcat(char *dest, char *src)
+ {
+    int c = 0, j = 0;
+    
+    while (dest[c] != '\0')
+        c++;
+    while (src[j] != '\0')
+    {
+        dest[c] = src[j];
+        j++;
+        c++;
+    }
+    dest[c] = '\0';
+    return(dest);
+ }
