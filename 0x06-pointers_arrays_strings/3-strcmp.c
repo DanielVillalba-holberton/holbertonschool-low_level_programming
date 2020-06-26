6d9fd33462915d0983fe7d89c5c0ee6b56eea2ec
Daniel Villalba <1821@holbertonschool.com>
2020-06-26 01:53:52 +0000

    fixed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -11,10 +11,13 @@ int _strcmp(char *s1, char *s2)
 	int i = 0, j = 0, r;
 
 	while (s1[i] != '\0' && s2[i] != '\0')
+	{
 		if (s1[i] != s2[i])
 		{
 			r = s1[i] - s2[i];
 			return (r);
 		}
+		i++;
+	}
 	return (0);
 }
