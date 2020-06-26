e545f814a8d4d96319226325fd82824a21ebae22
Daniel Villalba <1821@holbertonschool.com>
2020-06-26 02:08:23 +0000

    fixed

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -12,24 +12,25 @@ char *cap_string(char *s)
 	while (s[i] != '\0')
 	{
 		switch (s[i])
-        {
-            case ' ':
-            case '\t':
-            case '\n':
-            case ',':
-            case ';':
-            case '.':
-            case '!':
-            case '?':
-            case '"':
-            case '(':
-            case ')':
-            case '{':
-            case '}':
-            if (s[i + 1] <123 && s[i + 1] > 96)
+		{
+			case ' ':
+			case '\t':
+			case '\n':
+			case ',':
+			case ';':
+			case '.':
+			case '!':
+			case '?':
+			case '"':
+			case '(':
+			case ')':
+			case '{':
+			case '}':
+			if (s[i + 1] < 123 && s[i + 1] > 96)
 				s[i + 1] = s[i + 1] - 32;
-        }
-        i++;
+		}
+		i++;
 	}
 	return (s);
 }
+
