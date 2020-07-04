a5684b22802b15093e5efa5d4199ac8101ee22e2
Daniel Villalba <1821@holbertonschool.com>
2020-07-04 20:49:16 +0000

    fixed

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -9,25 +9,26 @@
  */
 int main(int argc, char *argv[])
 {
-    int i, j = 0, sum;
-    if (argc == 1)
-        {
-        printf("0\n");
-        return (0);
-        }
-    for (i = 1; i < argc; i++)
-    {
-        while (argv[i][j] != '\0')
-        {
-            if (!isdigit(argv[i][j]))
-            {
-                printf("Error\n");
-                return (1);
-            }
-            j++;
-        }
-        sum = sum + atoi(argv[i]);
-    }
-    printf("%i\n", sum);
-    return (0);
+	int i, j = 0, sum;
+
+	if (argc == 1)
+		{
+		printf("0\n");
+		return (0);
+		}
+	for (i = 1; i < argc; i++)
+	{
+		while (argv[i][j] != '\0')
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+			j++;
+		}
+		sum = sum + atoi(argv[i]);
+	}
+	printf("%i\n", sum);
+	return (0);
 }
