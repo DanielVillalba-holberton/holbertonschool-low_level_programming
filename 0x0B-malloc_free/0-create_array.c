43f07aa8cff9844528447dbc2b3871c8152f1151
DanielVillalba-holberton <1821@holbertonschool.com>
2020-07-07 17:55:18 +0000

    fixed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <stdio.h>
 /**
- * create_array - function that creates an array of chars
+ * create_array - function that creates an array of chars.
  * @size: size of the array
  * @c: char to be printed
  *
