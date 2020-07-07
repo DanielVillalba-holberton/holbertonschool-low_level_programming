3ce5e09fb162c97de4c1e3c66acd7f16b535c7b2
DanielVillalba-holberton <1821@holbertonschool.com>
2020-07-07 20:00:20 +0000

    fixed

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -15,6 +15,6 @@ char *create_array(unsigned int size, char c)
 	array = malloc(sizeof(char) * size);
 	for (i = 0; i < size; i++)
 		array[i] = c;
-	array[i] = '\0';
-	return (size < 1 ? '\0' : array);
+	array[i] = 0;
+	return (size < 1 ? 0 : array);
 }
