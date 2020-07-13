a1a20b02041f9ba1c7d03798be452d0775fba2ad
DanielVillalba-holberton <1821@holbertonschool.com>
2020-07-13 18:03:58 +0000

    fixed

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,4 +1,4 @@
-#ifndef _3_FUNCTIONS_LIKE_MACRO_H
-#define _3_FUNCTIONS_LIKE_MACRO_H
+#ifndef _3_FUNCTIONS_LIKE_MACRO_H_
+#define _3_FUNCTIONS_LIKE_MACRO_H_
 #define ABS(x) (x > 0 ? x : x * (-1))
 #endif
