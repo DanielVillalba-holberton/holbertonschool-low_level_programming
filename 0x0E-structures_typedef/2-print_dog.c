682e7eba8a79917c7eaf710e78e6056bfcadfb3a
DanielVillalba-holberton <1821@holbertonschool.com>
2020-07-13 19:19:42 +0000

    fixed

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -10,7 +10,7 @@ void print_dog(struct dog *d)
 	if (d)
 	{
 		printf("Name: %s\n", d->name == NULL ? "(nil" : d->name);
-		printf("Age: %f\n", d->age);
+		printf("Age: %.6f\n", d->age);
 		printf("Owner: %s\n", d->owner == NULL ? "(nil" : d->owner);
 	}
 }
