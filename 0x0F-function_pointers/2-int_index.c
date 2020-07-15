79dfd09b3595813a04597dcd36f954fa9462433f
DanielVillalba-holberton <1821@holbertonschool.com>
2020-07-15 22:30:15 +0000

    fixed

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -10,11 +10,14 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i = 0;
 
-	while (i < size)
+	if (array && size && cmp)
 	{
+		while (i < size)
+		{
 		if (cmp(array[i]))
 			return (i);
 		i++;
+		}
 	}
 	return (-1);
 }
