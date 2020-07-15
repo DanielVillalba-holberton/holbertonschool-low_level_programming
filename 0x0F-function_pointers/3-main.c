153b01b5a44f6d366f159c3c6989553c13d08173
DanielVillalba-holberton <1821@holbertonschool.com>
2020-07-15 22:36:13 +0000

    fixed

--- 0x0F-function_pointers/3-main.c
+++ 0x0F-function_pointers/3-main.c
@@ -8,12 +8,26 @@
  */
 int main(int argc, char **argv)
 {
-	if (argc == 4)
+	int (*oper)(int a, int b);
+
+	if (argc != 4)
 	{
-		printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
-		return (0);
+		printf("Error\n");
+		return (98);
 	}
-	printf("Error\n");
-	return (98);
+	if ((*argv[2] == '/' || *argv[2] == '%') && *argv[3] == '0')
+	{
+		printf("Error\n");
+		return (100);
+	}
+	oper = get_op_func(argv[2]);
+
+	if (!oper)
+	{
+		printf("Error\n");
+		return (99);
+	}
+	printf("%d\n", get_op_func(argv[2])(atoi(argv[1]), atoi(argv[3])));
+	return (0);
 }
 
