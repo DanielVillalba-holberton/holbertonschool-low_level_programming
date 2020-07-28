eb820c9c5880843d77f47e2b25300c40369ff949
DanielVillalba-holberton <1821@holbertonschool.com>
2020-07-28 14:03:18 +0000

    frees a listint_t list fixed

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -7,13 +7,10 @@
 
 void free_listint2(listint_t **head)
 {
-	if (head)
+	if (head != NULL && *head != NULL)
 	{
-		while (*head)
-		{
-			head = &(*head)->next;
-			free(*head);
-			*head = NULL;
-		}
+		free_listint2(&(*head)->next);
+		free(*head);
+		*head = NULL;
 	}
 }
