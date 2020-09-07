c8281b26be5570f7b466f5768cded05e2659f0d2
DanielVillalba-holberton <1821@holbertonschool.com>
2020-09-07 22:30:43 +0000

    test another way

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -P /tmp https://github.com/DanielVillalba-holberton/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/orn.so
-export LD_PRELOAD=/tmp/orn.so
+wget -P /tmp https://github.com/DanielVillalba-holberton/holbertonschool-low_level_programming/raw/master/0x18-dynamic_libraries/onr.so
+export LD_PRELOAD=/tmp/onr.so
