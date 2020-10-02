ce5766552225f742ee53f9b6305c48643835aebc
DanielVillalba-holberton <1821@holbertonschool.com>
2020-10-02 15:37:30 +0000

    fixed condition to return NULL

--- 0x1A-hash_tables/4-hash_table_get.c
+++ 0x1A-hash_tables/4-hash_table_get.c
@@ -10,6 +10,9 @@ char *hash_table_get(const hash_table_t *ht, const char *key)
 	unsigned long int index;
 	hash_node_t *node;
 
+	if (ht == NULL || !key)
+		return (NULL);
+
 	index = key_index((unsigned char *)key, ht->size);
 	if (ht->array == NULL || (ht->array)[index] == NULL)
 		return (NULL);
